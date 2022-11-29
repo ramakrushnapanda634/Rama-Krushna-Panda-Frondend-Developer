ad5cfbf6a5f33fa3a76060977ad760b777251948
ramakrushnapanda634 <ramakrushnapanda634@gmail.com>
2022-11-29 12:37:19 +0530

    minor change

--- clint/src/components/Product.jsx
+++ clint/src/components/Product.jsx
@@ -8,26 +8,28 @@ import Box from "@mui/material/Box";
 import axios from "axios";
 import Pagination from "./Pagination";
 const Sample = () => {
-  const [data, setData] =useState([]);
+  const [data, setData] = useState([]);
   const [filterVal, setFilterVal] = useState([]);
-   const [common, setcommon] = useState([]);
-   const [popup, setPopup] = useState([]);
-   const [togle, setTogle] = useState(false);
-   const [showPerPage, setShowPerPage] = useState(21);
-   const [pagination, setPagination] = useState({
-     start: 0,
-     end: showPerPage,
-   });
-   const onPaginationChange = (start, end) => {
-     setPagination({ start: start, end: end });
-   };
+  const [common, setcommon] = useState([]);
+  const [popup, setPopup] = useState([]);
+  const [togle, setTogle] = useState(false);
+
+  const [showPerPage, setShowPerPage] = useState(8);
+  const [pagination, setPagination] = useState({
+    start: 0,
+    end: showPerPage,
+  });
+  const onPaginationChange = (start, end) => {
+    setPagination({ start: start, end: end });
+  };
+  //http://localhost:8080/api/products
   useEffect(() => {
     axios
-      .get("http://localhost:8080/api/products")
+      .get("https://api.spacexdata.com/v3/capsules")
       .then((res) => {
-        // console.log(res.data.products)
-        setData(res.data.products);
-         setcommon(res.data.products);
+        console.log(res.data);
+        setData(res.data);
+        setcommon(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -38,35 +40,37 @@ const Sample = () => {
   };
 
   const h2l = () => {
-    const filteredB = [...common].sort((a, b) => b.population - a.population);
+    const filteredB = [...common].sort((a, b) => b.reuse_count - a.reuse_count);
     console.log(filteredB);
     setData(filteredB);
   };
   const l2h = () => {
-    const filtered = [...common].sort((a, b) => a.population - b.population);
+    const filtered = [...common].sort((a, b) => a.reuse_count - b.reuse_count);
     console.log(filtered);
 
     setData(filtered);
   };
-const handleChange = (e) => {
-  const filterResult = data.filter(
-    (item) =>
-      item.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
-      item.country.toLowerCase().includes(e.target.value.toLowerCase())
-  );
+  const handleChange = (e) => {
+    const filterResult = data.filter(
+      (item) =>
+        item.status.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.type.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.original_launch(e.target.value)
+    );
 
-  setData(filterResult);
-  setFilterVal(e.target.value);
-};
+    setData(filterResult);
+    setFilterVal(e.target.value);
+  };
   return (
-    <div>
+    <div  >
       <Box>
-        <h3>Sort By Price</h3>
+        <h1>Sort By Reuse_Count</h1>
         <Button onClick={h2l}>High To low</Button>
         <Button onClick={l2h}>Low to High</Button>
       </Box>
-      <p>Search for Country or Name</p>
+      <h1>Search for Status  or Type</h1>
       <Input type="searh" value={filterVal} onChange={(e) => handleChange(e)} />
+
       <Grid
         container
         rowSpacing={1}
@@ -85,9 +89,12 @@ const handleChange = (e) => {
                 <img src={item.image} alt="" width={150}></img>
               </Box>
 
-              <Box> Name:{item.name}</Box>
-              <Box>Country:{item.country}</Box>
-              <Box>Population:{item.population}</Box>
+              <Box> Capsule_serial:{item.capsule_serial}</Box>
+              <Box>Capsule_id:{item.capsule_id}</Box>
+              <Box>Status:{item.status}</Box>
+              <Box>Original_launch:{item.original_launch}</Box>
+              <Box>Type:{item.type}</Box>
+              <Box>Reuse_Count:{item.reuse_count}</Box>
               <Box component="span" sx={{ p: 0.5, border: "1px dashed red" }}>
                 <Button onClick={() => changeContent(item)}>
                   More Details Page
@@ -106,12 +113,11 @@ const handleChange = (e) => {
             <Grid className="pop_up_content">
               {popup.map((item, index) => (
                 <Grid key={index}>
-                  <Box>
-                    <img src={item.image} alt="flag" width={100} />
-                  </Box>
-                  <Box>Name{item.name}</Box>
-                  <Box>Country:{item.country}</Box>
-                  <Box>Population:{item.population}</Box>
+                  <Box> Capsule_serial:{item.capsule_serial}</Box>
+                  <Box>Capsule_id:{item.capsule_id}</Box>
+                  <Box>Status:{item.status}</Box>
+                  <Box>Original_launch:{item.original_launch}</Box>
+                  <Box>Type:{item.type}</Box>
                 </Grid>
               ))}
             </Grid>
